06e8b62a85a766df851fef79b1050e2933ed4881
Tanay Khandke <tanutd.khandke@gmail.com>
2019-06-10 11:06:40 -0700

    test second commit added listeners

---
+++ src/main/java/flexon/com/practest/TestNGPractice/ListenersPrac.java
@@ -0,0 +1,10 @@
+package flexon.com.practest.TestNGPractice;
+
+public class ListenersPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("learning listeners");
+	}
+
+}
